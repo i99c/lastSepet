461a74225a286b2fd4fff6a01c302ea19607330f
Ipek Canli <119710326+i99c@users.noreply.github.com>
2023-11-24 13:35:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ if (localItem) {
 console.log(window.location.href);
 console.log(sepet.length);
 
-if (window.location.href == "http://127.0.0.1:5500/index.html") {
+if (window.location.href == "https://i99c.github.io/lastSepet/sepet.html") {
   const row = document.querySelector(".row");
   const input = document.getElementById("input");
 
