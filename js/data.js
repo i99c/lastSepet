54a5ee94955f2c037427303fac7869f9a417683d
unknown <ipekcanli2014@gmail.com>
2023-11-24 10:15:15 +0300

    first commit

---
+++ js/data.js
@@ -0,0 +1,30 @@
+let urunler = [
+    {
+        "isim": "Pamuk Gömlek",
+        "açıklama": "Rahat ve nefes alabilir pamuk gömlek",
+        "fiyat": 79.99,
+        "fotoğraf": "https://picsum.photos/id/65/200/300",
+        "stok": 150
+    },
+    {
+        "isim": "Keten Pantolon",
+        "açıklama": "Şık ve rahat keten pantolon",
+        "fiyat": 129.99,
+        "fotoğraf": "https://picsum.photos/id/35/200/300",
+        "stok": 100
+    },
+    {
+        "isim": "Deri Ceket",
+        "açıklama": "Klasik tarzda deri ceket",
+        "fiyat": 199.99,
+        "fotoğraf": "https://picsum.photos/id/238/200/300",
+        "stok": 75
+    },
+    {
+        "isim": "Kot Şort",
+        "açıklama": "Yaz ayları için rahat kot şort",
+        "fiyat": 49.99,
+        "fotoğraf": "https://picsum.photos/id/278/200/300",
+        "stok": 120
+    }
+]
